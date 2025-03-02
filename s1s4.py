9a34ccfa2214d2056b4e15b3f0d1e738f36e085c
mahshad-m88 <mahshadchoopani278@gmail.com>
2025-03-02 21:39:44 +0330

    Add files via upload

---
+++ s1s4.py
@@ -0,0 +1,7 @@
+a=float(input("enter number 1"))
+b=float(input("enter number 2"))
+c=float(input("enter number 3"))
+if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c++2 == a**2:
+    print("yes")
+else:
+    print("no")
