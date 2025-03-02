332c7d4b7da6c5b90190983c4343cfc44d82549f
mahshad-m88 <mahshadchoopani278@gmail.com>
2025-03-02 21:53:46 +0330

    Add files via upload

---
+++ s1s1.py
@@ -0,0 +1,10 @@
+score = int(input("Enter score: "))  
+
+if score >= 95:  # ا
+    print("عالی")
+elif 75 <= score < 95:
+    print("خوب")
+elif 50 <= score < 75:
+    print("قابل قبول")
+else:
+    print("مردود")
